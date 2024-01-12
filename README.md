aa41881bb2b01c30d1a1e0bb07868df8b27ddfc3
andreweryan <andrrvt15@gmail.com>
2024-01-11 23:36:49 -0500

    adding to gitignore, update todo

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
 # Python Repo template
 
-List
-- Item 1
-- Item 2
-- Item 3
-
-Task List
-- [x] Task 1
-- [ ] Task 2
-- [x] Task 3
-
-Numbered List
-1. Item 1
-2. Item 2
-3. Item 3
+To do:
+- [ ] Update README with installation, usage, etc. 
+- [ ] Write unit tests
+- [ ] Add CLI arg for specifying lat long vs long lat
 
 ```python
 
-def main():
-    print("Hello, World!")
 
 ```
--- geodist/cli.py
+++ geodist/cli.py
@@ -1,22 +1,33 @@
 import argparse
-from .haversine import *
+from haversine import haversine
+
 
 def main():
-
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--coordinates', nargs='*', required=True, help=" ")
-    parser.add_argument('--unit', type=str, default='km', help="Earth unit for Haversine distance metric.")
+    parser.add_argument(
+        "--coordinates",
+        nargs="*",
+        required=True,
+        help="Latitude1 Longitude1 Latitude2 Longitude2",
+    )
+    parser.add_argument(
+        "--unit",
+        type=str,
+        default="km",
+        help="Earth unit for Haversine distance metric.",
+    )
 
     args = parser.parse_args()
 
     coords = list(map(float, args.coordinates))
 
     distance = haversine(coords[0], coords[1], coords[2], coords[3], args.unit)
+
     print(distance)
 
+
 if __name__ == "__main__":
-    
     """
     TODO:
     - write unit tests
@@ -27,5 +38,5 @@ if __name__ == "__main__":
     mi: 1.613, km: 2.595, ft: 8516.349, m: 2595.783
 
     """
-    
+
     main()
