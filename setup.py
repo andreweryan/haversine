9436e4bf6683e27e0da0f818e31595c91abd3475
andreweryan <andrrvt15@gmail.com>
2024-01-16 01:24:49 -0500

    updating setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="geodist",
+    name="haversine",
     version="0.0.1",
     author="Andrew Ryan",
     author_email="aryanvt15@proton.me",
