5b5e50e267ca46ddb30de37807282a025038f92f
andreweryan <andrrvt15@gmail.com>
2024-01-17 16:03:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="geodist",
+    name="haversine",
     version="0.0.1",
     author="Andrew Ryan",
     author_email="aryanvt15@proton.me",
     description="Tool for calculating haversine distance between two geospatial coordinates.",
     packages=find_packages(),
     entry_points={
-        "console_scripts": ["geodist = haversine.cli:main"],
+        "console_scripts": ["haversine = haversine.cli:main"],
     },
 )
