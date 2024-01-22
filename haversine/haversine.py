51b7f38e9c1041499b87692416b39f5563869913
andreweryan <andrrvt15@gmail.com>
2024-01-22 11:58:45 -0500

    adding unit map vs if/else statements

--- haversine/haversine.py
+++ haversine/haversine.py
@@ -4,8 +4,7 @@ from math import sqrt, sin, cos, asin, radians
 def haversine_distance(
     lat1: float, lon1: float, lat2: float, lon2: float, unit: str = "kilometers"
 ) -> float:
-    """
-    Calculate the Haversine (great-circle) distance between two geospatial coordinates
+    """Calculate the Haversine (great-circle) distance between two geospatial coordinates.
 
     Args:
         lat1 (float): Point 1 Latitude
@@ -16,32 +15,31 @@ def haversine_distance(
     Returns:
         distance (float): Haversine Distance between two points
     """
+    
+    unit_map = {
+        "kilometers": 6371.009,
+        "meters": 6371009,
+        "miles": 3958.7614581,
+        "feet": 20902260.49876800925,
+    }
 
-    if unit == "miles":
-        units = 3959.87433
-    elif unit == "feet":
-        units = 20908136.4624
-    elif unit == "kilometers":
-        units = 6372.8
-    elif unit == "meters":
-        units = 6372800
-    else:
+    r = unit_map.get(unit)
+    
+    if not r:
         raise ValueError("Units not specified.")
 
-    r = units
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
 
-    dLat = radians(lat2 - lat1)
+    dlat = lat2 - lat1
+    
+    dlon = lon2 - lon1
 
-    dLon = radians(lon2 - lon1)
+    h = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    
+    h = min(1, h)
+    
+    arc = 2 * asin(sqrt(h))
 
-    lat1 = radians(lat1)
-
-    lat2 = radians(lat2)
-
-    a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
-
-    c = 2 * asin(sqrt(a))
-
-    distance = r * c
+    distance = arc * r
 
     return distance
